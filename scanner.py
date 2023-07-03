b2f023d92121c9ea65390aa57d8d32db95cbeed4
codered072 <kali@ip-172-31-80-144.ec2.internal>
2023-07-03 00:28:58 +0000

    adds keyboardinterrupt

--- scanner.py
+++ scanner.py
@@ -17,30 +17,34 @@ def tryLogin(fileName):
         except Exception as e:
             print(f'{ipAddress} anonymous login FAILED.')
 
-parser = argparse.ArgumentParser(
-        prog='',
-        description='Anonymous FTP scanner',
-        add_help=True)
 
-parser.add_argument("-i", type=str, required=False, help="Single IP to scan")
-parser.add_argument("-l", type=str, required=False, help="List of IPs to scan")
-options = parser.parse_args()
-# Check against one host
-if options.i:
-    ipAddr = options.i
-    try:
-        ftp = ftplib.FTP(ipAddr)
-        ftp.login('anonymous', '')
-        print(f'{ipAddr} anonymous login *SUCCESS*.')
-        ftp.quit()
-    except Exception as e:
-        print(f'{ipAddr} anonymous login FAILED.')
+try:
+    parser = argparse.ArgumentParser(
+            prog='',
+            description='Anonymous FTP scanner',
+            add_help=True)
 
-# If IP list file, pass file name to function
-if options.l:
-    tryLogin(str(options.l))
+    parser.add_argument("-i", type=str, required=False, help="Single IP to scan")
+    parser.add_argument("-l", type=str, required=False, help="List of IPs to scan")
+    options = parser.parse_args()
+    # Check against one host
+    if options.i:
+        ipAddr = options.i
+        try:
+            ftp = ftplib.FTP(ipAddr)
+            ftp.login('anonymous', '')
+            print(f'{ipAddr} anonymous login *SUCCESS*.')
+            ftp.quit()
+        except Exception as e:
+            print(f'{ipAddr} anonymous login FAILED.')
 
-#If no options selected, stop
-if not any(vars(options).values()):
-    parser.error("No arguments provided. Must either enter one IP or list of IPs to scan with options `-i` for single IP address or `-l` for a list of IP addresses.")
+    # If IP list file, pass file name to function
+    if options.l:
+        tryLogin(str(options.l))
 
+    #If no options selected, stop
+    if not any(vars(options).values()):
+        parser.error("No arguments provided. Must either enter one IP or list of IPs to scan with options `-i` for single IP address or `-l` for a list of IP addresses.")
+
+except KeyboardInterrupt:
+    print("\nStopped by user")
